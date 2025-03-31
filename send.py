bc42ca3d3809f66f3501486b3a628bc2a92244e0
athenova <aleksey.sergeyev@yandex.com>
2025-03-31 21:41:47 +0300

    Поправлены теги

--- send.py
+++ send.py
@@ -6,23 +6,23 @@ if __name__ == '__main__':
     tg = ProjectTelegram()
     vk = ProjectVk()
 
-    def job(type, chat_id=None, group_id=None):
-        tg.send(type=type, chat_id=chat_id)
+    def job(type, chat_id=None, group_id=None, tags=None):
+        tg.send(type=type, chat_id=chat_id, tags=tags)
         if group_id is not None:
-            vk.send(type=type, group_id=group_id, image_gen=False, text_gen=False)
+            vk.send(type=type, group_id=group_id, image_gen=False, text_gen=False, tags=tags)
 
-    schedule.every().day.at("21:00",'Europe/Moscow').do(job, type="pisces", chat_id='@pisces_the', group_id='229837683', tags=['#гороскоп', '#таро', '#тароназавтра', '#рыбы'])
-    schedule.every().day.at("21:01",'Europe/Moscow').do(job, type="aries", chat_id='@aries_the', group_id='229837854', tags=['#гороскоп', '#таро', '#тароназавтра', '#овен'])
-    schedule.every().day.at("21:02",'Europe/Moscow').do(job, type="taurus", group_id='229860740', tags=['#гороскоп', '#таро', '#тароназавтра', '#телец'])
-    schedule.every().day.at("21:03",'Europe/Moscow').do(job, type="gemini", chat_id='@gemini_the', group_id='229837895', tags=['#гороскоп', '#таро', '#тароназавтра', '#близнецы'])
-    schedule.every().day.at("21:04",'Europe/Moscow').do(job, type="cancer", group_id='229860780', tags=['#гороскоп', '#таро', '#тароназавтра', '#рак'])
-    schedule.every().day.at("21:05",'Europe/Moscow').do(job, type="leo", group_id='229860665', tags=['#гороскоп', '#таро', '#тароназавтра', '#лев'])
-    schedule.every().day.at("21:06",'Europe/Moscow').do(job, type="virgo", group_id='229860810', tags=['#гороскоп', '#таро', '#тароназавтра', '#дева'])
-    schedule.every().day.at("21:07",'Europe/Moscow').do(job, type="libra", group_id='229860834', tags=['#гороскоп', '#таро', '#тароназавтра', '#весы'])
-    schedule.every().day.at("21:08",'Europe/Moscow').do(job, type="scorpio", group_id='229860866', tags=['#гороскоп', '#таро', '#тароназавтра', '#скорпион'])
-    schedule.every().day.at("21:09",'Europe/Moscow').do(job, type="sagittarius", group_id='229860894', tags=['#гороскоп', '#таро', '#тароназавтра', '#стрелец'])
-    schedule.every().day.at("21:10",'Europe/Moscow').do(job, type="capricorn", chat_id='@capricorn_the', group_id='229837876', tags=['#гороскоп', '#таро', '#тароназавтра', '#козерог'])
-    schedule.every().day.at("21:11",'Europe/Moscow').do(job, type="aquarius", chat_id='@aquarius_the', group_id='229837930', tags=['#гороскоп', '#таро', '#тароназавтра', '#водолей'])
+    schedule.every().day.at("21:40",'Europe/Moscow').do(job, type="pisces", chat_id='@pisces_the', group_id='229837683', tags=['#гороскоп', '#таро', '#тароназавтра', '#рыбы'])
+    schedule.every().day.at("21:41",'Europe/Moscow').do(job, type="aries", chat_id='@aries_the', group_id='229837854', tags=['#гороскоп', '#таро', '#тароназавтра', '#овен'])
+    schedule.every().day.at("21:42",'Europe/Moscow').do(job, type="taurus", group_id='229860740', tags=['#гороскоп', '#таро', '#тароназавтра', '#телец'])
+    schedule.every().day.at("21:43",'Europe/Moscow').do(job, type="gemini", chat_id='@gemini_the', group_id='229837895', tags=['#гороскоп', '#таро', '#тароназавтра', '#близнецы'])
+    schedule.every().day.at("21:44",'Europe/Moscow').do(job, type="cancer", group_id='229860780', tags=['#гороскоп', '#таро', '#тароназавтра', '#рак'])
+    schedule.every().day.at("21:45",'Europe/Moscow').do(job, type="leo", group_id='229860665', tags=['#гороскоп', '#таро', '#тароназавтра', '#лев'])
+    schedule.every().day.at("21:46",'Europe/Moscow').do(job, type="virgo", group_id='229860810', tags=['#гороскоп', '#таро', '#тароназавтра', '#дева'])
+    schedule.every().day.at("21:47",'Europe/Moscow').do(job, type="libra", group_id='229860834', tags=['#гороскоп', '#таро', '#тароназавтра', '#весы'])
+    schedule.every().day.at("21:48",'Europe/Moscow').do(job, type="scorpio", group_id='229860866', tags=['#гороскоп', '#таро', '#тароназавтра', '#скорпион'])
+    schedule.every().day.at("21:49",'Europe/Moscow').do(job, type="sagittarius", group_id='229860894', tags=['#гороскоп', '#таро', '#тароназавтра', '#стрелец'])
+    schedule.every().day.at("21:50",'Europe/Moscow').do(job, type="capricorn", chat_id='@capricorn_the', group_id='229837876', tags=['#гороскоп', '#таро', '#тароназавтра', '#козерог'])
+    schedule.every().day.at("21:51",'Europe/Moscow').do(job, type="aquarius", chat_id='@aquarius_the', group_id='229837930', tags=['#гороскоп', '#таро', '#тароназавтра', '#водолей'])
 
     fifteen_minutes = 15 * 60
 
